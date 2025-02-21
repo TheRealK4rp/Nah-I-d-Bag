296d9d713a0567bbbabaa056862381753057dcd2
TheRealK4rp <joeljobtsang@gmail.com>
2025-02-21 15:29:08 -0800

    removed useless comments

--- ResizableArrayBag.java
+++ ResizableArrayBag.java
@@ -18,11 +18,12 @@ public class ResizableArrayBag<T> implements BagInterface<T>{
         bag = tBag;
     }
 
+    @Override
     public int size(){
         return size;
     }
 
-    //error handling needed for half these lol
+    @Override
     public boolean add(T item){
         isFull();
         bag[size] = item;
@@ -30,7 +31,7 @@ public class ResizableArrayBag<T> implements BagInterface<T>{
         return true;
     }
 
-    //removes one of a certain item
+    @Override
     public boolean remove(T item){
         boolean removed = false;
         for(int i = 0;i < size;i++){
@@ -44,8 +45,7 @@ public class ResizableArrayBag<T> implements BagInterface<T>{
         return removed;
     }
 
-    //removes one random item
-    // something
+    @Override
     public T remove(){
         if(isEmpty()) throw new NullPointerException("Bag Empty");
         T item = bag[size-1];
@@ -54,11 +54,12 @@ public class ResizableArrayBag<T> implements BagInterface<T>{
         return item;
     }
 
+    @Override
     public boolean isEmpty(){
         return size <= 0;
     }
 
-    // this should work.... just allocates more memory for the list // might be better to multiply the size but eh idk
+    
     public void isFull(){
         if(size == hiddenSize){
             hiddenSize *= 2;
@@ -74,7 +75,6 @@ public class ResizableArrayBag<T> implements BagInterface<T>{
         }
     }
 
-    // just fyi this can only return Object[] array
     public T[] toArray(){
         @SuppressWarnings("unchecked")
         T[] tBag = (T[]) new Object[size];
@@ -85,7 +85,7 @@ public class ResizableArrayBag<T> implements BagInterface<T>{
     }
 
 
-    // everything below this comment is untested
+    @Override
     public void clear(){
         @SuppressWarnings("unchecked")
         T[] tBag = (T[]) new Object[hiddenSize];
@@ -94,6 +94,7 @@ public class ResizableArrayBag<T> implements BagInterface<T>{
         size = 0;
     }
 
+    @Override
     public int getFrequencyOf(T item){
         int count = 0;
         for(int i = 0;i < size;i++){
@@ -103,7 +104,8 @@ public class ResizableArrayBag<T> implements BagInterface<T>{
         }
         return count;
     }
-
+    
+    @Override
     public boolean contains(T item){
         for(int i = 0;i < size;i++){
             if(item == bag[i]){
